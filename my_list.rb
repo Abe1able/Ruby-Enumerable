d601fa7094ffa58998367124c3be0b4ad67ca676
heinthant <hthant00chk@gmail.com>
2023-03-10 11:12:21 +0630

    Make required changes
    - Use require_relative import
    - Modify #each method
    - Call methods to test

--- my_list.rb
+++ my_list.rb
@@ -1,13 +1,28 @@
-require './my_enumerable'
+require_relative './my_enumerable'
 
 class MyList
   def initialize(*list)
-    @list = list
+    @list = []
+    list.each { |item| @list << item }
   end
 
   include MyEnumerable
 
-  def each(&block)
-    @list.each(&block)
+  def each()
+    return to_enum(:each) unless block_given?
+
+    @list.length.times do |i|
+      yield(@list[i])
+    end
   end
 end
+
+# Testing the methods
+
+list = MyList.new(1, 2, 3, 4)
+
+puts "All items < 5: #{list.all? { |e| e < 5 }}"
+puts "All items > 5: #{list.all? { |e| e > 5 }}"
+puts "List contains 2: #{list.any? { |e| e == 2 }}"
+puts "List contains 5: #{list.any? { |e| e == 5 }}"
+puts "Even numbers: #{list.filter(&:even?)}"
